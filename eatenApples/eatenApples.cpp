537a4024070c596bf1f268f13cd483ea7776daa6
tcxia <muctianciking@163.com>
2021-12-23 22:09:44 +0000

    2021-12-24

---
+++ eatenApples/eatenApples.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    int eatenApples(vector<int>& apples, vector<int>& days) {
+    }
+};
---
+++ searchRange/searchRange.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+    }
+};
