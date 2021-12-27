1d9b7e938e7d70d10704d009e9c81bc14d9d39de
tcxia <muctianciking@163.com>
2021-12-27 16:01:03 +0800

    2021-12-27

--- rightSideView/rightSideView.cpp
+++ rightSideView/rightSideView.cpp
@@ -31,4 +31,21 @@ class Solution {
         }
         return result;
     }
+
+
+    vector<int> rightSideViewv2(TreeNode *root){
+        queue<TreeNode*> que;
+        if(root != NULL) que.push(root);
+        while(!que.empty()){
+            int size = que.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* node = que.front();
+                que.pop();
+                if(i == size - 1) result.push_back(node->val);
+                if(node->left) que.push(node->left);
+                if(node->right) que.push(node->right);
+            }
+        }
+        return result;
+    }
 };
