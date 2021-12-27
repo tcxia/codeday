6e6fe7b34189c30b4713958eb49d75f02e1f8675
tcxia <muctianciking@163.com>
2021-12-27 03:08:30 +0000

    2021-12-27

--- numFriendRequests/numFriendRequests.cpp
+++ numFriendRequests/numFriendRequests.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 
 class Solution {
+    vector<vector<int>> result;
+    vector<int> path;
+
+    bool isFriend(vector<int> ages, int x, int y) {
+        if ((ages[y] <= (0.5 * ages[x] + 7)) || (ages[y] > ages[x]) || (ages[y] > 100 && ages[x] < 100)){
+            return false;
+        }
+        return true;
+    }
+
+    void backtracking(vector<int>& ages) {
+        if (path.size() == 2) {
+            result.push_back(path);
+        }
+
+        for (int i = 0; i < ages.size(); i++) {
+            
+            
+        }
+    }
+
    public:
     int numFriendRequests(vector<int>& ages) {
     }
