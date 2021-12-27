699b83a915a0fa11f1fe4c216802fc596d032715
tcxia <muctianciking@163.com>
2021-12-27 14:04:05 +0800

    2021-12-27

--- numFriendRequests/numFriendRequests.cpp
+++ numFriendRequests/numFriendRequests.cpp
@@ -6,25 +6,56 @@ class Solution {
     vector<vector<int>> result;
     vector<int> path;
 
-    bool isFriend(vector<int> ages, int x, int y) {
-        if ((ages[y] <= (0.5 * ages[x] + 7)) || (ages[y] > ages[x]) || (ages[y] > 100 && ages[x] < 100)){
+    bool isFriend(int x, int y) {
+        if ((y<= (0.5 * x + 7)) || (y > x) || (y > 100 && x < 100)){
             return false;
         }
         return true;
     }
 
-    void backtracking(vector<int>& ages) {
+    void backtracking(vector<int>& ages, vector<bool>& used) {
         if (path.size() == 2) {
-            result.push_back(path);
+            if (isFriend(path[0], path[1])){
+                result.push_back(path);
+                return;
+            } 
         }
 
         for (int i = 0; i < ages.size(); i++) {
-            
-            
+            if (used[i]){
+                continue;
+            }
+                
+            path.push_back(ages[i]);
+            used[i] = true;
+
+            backtracking(ages, used);
+
+            path.pop_back();
+            used[i] = false;
         }
     }
 
    public:
-    int numFriendRequests(vector<int>& ages) {
+    vector<vector<int>> numFriendRequests(vector<int>& ages) {
+        result.clear();
+        path.clear();
+
+        vector<bool> used(ages.size(), false);
+    
+        backtracking(ages, used);
+
+        return result;
+
     }
 };
+
+int main(){
+    vector<int> ages = {20,30,100,110,120};
+    Solution solve;
+    vector<vector<int>> result = solve.numFriendRequests(ages);
+    for(auto& res: result){
+        cout << res[0] << "," << res[1] << endl;
+    }
+    
+}
