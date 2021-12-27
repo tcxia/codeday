2e6517eae12922e744637dd25d052e3689e0f232
tcxia <muctianciking@163.com>
2021-12-27 10:46:09 +0000

    2021-12-27

--- largestValues/largestValues.cpp
+++ largestValues/largestValues.cpp
@@ -1,32 +1,34 @@
-#include <vector>
+#include <algorithm>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
     vector<int> result;
-public:
-    vector<int> largestValues(TreeNode* root) {
-        queue<TreeNode*> que;
-        if(root != NULL) que.push(root);
-        while(!que.empty()){
+
+   public:
+    vector<int> largestValues(TreeNode *root) {
+        queue<TreeNode *> que;
+        if (root != NULL) que.push(root);
+        while (!que.empty()) {
             int size = que.size();
             vector<int> temp;
-            for(int i = 0; i < size; i++){
-                TreeNode* node = que.front();
+            for (int i = 0; i < size; i++) {
+                TreeNode *node = que.front();
                 que.pop();
                 temp.push_back(node->val);
-                if(node->left) que.push(node->left);
-                if(node->right) que.push(node->right);
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
             }
             result.push_back(*max_element(temp.begin(), temp.end()));
         }
---
+++ maxDepth/maxDepth.cpp
@@ -0,0 +1,34 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    vector<vector<int>> result;
+    public:
+    int maxDepth(TreeNode *root) {
+        queue<TreeNode*> que;
+        if(root != NULL) que.push(root);
+        while(!que.empty()){
+            int size = que.size();
+            vector<int> temp;
+            for(int i = 0; i < size; i++){
+                TreeNode* node = que.front();
+                que.pop();
+                temp.push_back(node->val);
+                if(node->left) que.push(node->left);
+                if(node->right) que.push(node->right);
+            }
+            result.push_back(temp);
+        }
+        return result.size();
+    }
+};
