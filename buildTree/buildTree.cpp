b0d201d719c404132904215361df301a1e47c1b8
tcxia <muctianciking@163.com>
2021-12-31 18:09:22 +0800

    2021-12-31

---
+++ buildTree/buildTree.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+
+#include "../common/binaryTree.h"
+using namespace std;
+
+class Solution {
+   public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        TreeNode* node = new TreeNode(postorder[-1]);
+    }
+};
