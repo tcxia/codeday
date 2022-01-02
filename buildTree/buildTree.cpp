34e2882e399d5fce1aea4101fd7206afd8dc5174
tcxia <muctianciking@163.com>
2022-01-02 19:02:08 +0800

    2022-01-02

--- buildTree/buildTree.cpp
+++ buildTree/buildTree.cpp
@@ -5,9 +5,46 @@
 using namespace std;
 
 class Solution {
+    TreeNode* traversal(vector<int>& inorder, vector<int>& postorder) {
+        if (postorder.size() == 0) return NULL;
+
+        // 后序遍历数组最后一个元素，就是当前的中间节点
+        int rootValue = postorder[postorder.size() - 1];
+        TreeNode* root = new TreeNode(rootValue);
+
+        // 叶子节点
+        if (postorder.size() == 1) return root;
+
+        // 找到中序遍历的切割点
+        int delimiterIndex;
+        for (delimiterIndex = 0; delimiterIndex < inorder.size(); delimiterIndex++) {
+            if (inorder[delimiterIndex] == rootValue) break;
+        }
+
+        // 切割中序数组
+        // 左闭右开区间: [0, delimiterIndex]
+        vector<int> leftInorder(inorder.begin(), inorder.begin() + delimiterIndex);
+        // [delimiterIndex + 1, end]
+        vector<int> rightInorder(inorder.begin() + delimiterIndex + 1, inorder.end());
+
+        // posterder 舍弃最后一个元素
+        postorder.resize(postorder.size() - 1);
+
+        // 切割后序数组
+        // [0, leftIndex.size]
+        vector<int> leftPostorder(postorder.begin(), postorder.begin() + leftInorder.size());
+        // [leftIndex.size(), end]
+        vector<int> rightPostorder(postorder.begin() + leftInorder.size(), postorder.end());
+
+        root->left = traversal(leftInorder, leftPostorder);
+        root->right = traversal(rightInorder, rightPostorder);
+
+        return root;
+    }
+
    public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        TreeNode* node = new TreeNode(postorder[-1]);
-        
+        if (inorder.size() == 0 || postorder.size() == 0) return NULL;
+        return traversal(inorder, postorder);
     }
 };
--- mergeTrees/mergeTrees.cpp
+++ mergeTrees/mergeTrees.cpp
@@ -1,6 +1,32 @@
 #include "../common/binaryTree.h"
+
 class Solution {
+    TreeNode* mergeOnce(TreeNode* tree1, TreeNode* tree2) {
+        if (tree1 == nullptr && tree2 == nullptr)
+            return nullptr;
+        else if (tree1 != nullptr && tree2 == nullptr)
+            return tree1;
+        else if (tree1 == nullptr && tree2 != nullptr)
+            return tree2;
+
+        tree1->val += tree2->val;
+
+        mergeOnce(tree1->left, tree2->left);
+
+        mergeOnce(tree1->right, tree2->right);
+
+        return tree1;
+    }
+
    public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        // return mergeTrees(root1, root2);
+        if(root1 == nullptr) return root2;
+        if(root2 == nullptr) return root1;
+
+        root1->val += root2->val;
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+        return root1;
     }
 };
