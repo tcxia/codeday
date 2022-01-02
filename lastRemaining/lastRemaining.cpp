f1c867f1931709f0a71aaceb942508728a255347
tcxia <muctianciking@163.com>
2022-01-02 12:53:49 +0800

    2022-01-02

--- lastRemaining/lastRemaining.cpp
+++ lastRemaining/lastRemaining.cpp
@@ -1,6 +1,26 @@
-class Solution {
-public:
-    int lastRemaining(int n) {
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
 
+class Solution {
+   public:
+    int lastRemaining(int n) {
+        if(n == 1){
+            return 1;
+        }
+
+        if(n % 2 == 1){
+            return lastRemaining(n - 1);
+        }
+
+        return n + 2 - 2 * lastRemaining(n / 2);
+        
     }
 };
+
+int main() {
+    Solution solve;
+    int result = solve.lastRemaining(9);
+    cout << result << endl;
+}
