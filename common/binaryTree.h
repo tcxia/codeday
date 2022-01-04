35d9d3035c3abb810d389d8d9d6422b145be2617
tcxia <muctianciking@163.com>
2022-01-04 14:43:33 +0800

    2022-01-04

--- common/binaryTree.h
+++ common/binaryTree.h
@@ -2,12 +2,11 @@
 #include <vector>
 using namespace std;
 
-
-struct TreeNode{
+struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x): val(x), left(NULL), right(NULL){}
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 // struct TreeNode {
@@ -19,19 +18,18 @@ struct TreeNode{
 //     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 // };
 
-
-TreeNode* constructBinaryTree(const vector<int>& vec){
+TreeNode* constructBinaryTree(const vector<int>& vec) {
     vector<TreeNode*> vecTree(vec.size(), NULL);
     TreeNode* root = NULL;
-    for(int i = 0; i < vec.size(); i++){
+    for (int i = 0; i < vec.size(); i++) {
         TreeNode* node = NULL;
-        if(vec[i] != -1) node = new TreeNode(vec[i]);
+        if (vec[i] != -1) node = new TreeNode(vec[i]);
         vecTree[i] = node;
-        if(i == 0) root = node;
+        if (i == 0) root = node;
     }
 
-    for(int i = 0; i * 2 + 2 < vec.size(); i++){
-        if(vecTree[i] != NULL){
+    for (int i = 0; i * 2 + 2 < vec.size(); i++) {
+        if (vecTree[i] != NULL) {
             vecTree[i]->left = vecTree[i * 2 + 1];
             vecTree[i]->right = vecTree[i * 2 + 2];
         }
