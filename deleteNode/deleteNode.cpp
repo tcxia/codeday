0998960f7f62487d0fa16e75140df80e2e002f59
tcxia <muctianciking@163.com>
2022-01-07 18:22:20 +0800

    2022-01-07

--- deleteNode/deleteNode.cpp
+++ deleteNode/deleteNode.cpp
@@ -1,7 +1,37 @@
 #include "../common/binaryTree.h"
+
 class Solution {
    public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        
+        if(root == nullptr) return root;
+        if(root->val == key) {
+            if(root->left == nullptr && root->right == nullptr){
+                delete root;
+                return nullptr;
+            }
+            else if(root->left == nullptr){
+                TreeNode* retNode = root->right;
+                delete root;
+                return retNode;
+            }
+            else if(root->right == nullptr){
+                TreeNode* retNode = root->left;
+                delete root;
+                return retNode;
+            }else{
+                TreeNode* cur = root->right;
+                while(cur->left != nullptr){
+                    cur = cur->left;
+                }
+                cur->left = root->left;
+                TreeNode* tmp = root;
+                root = root->right;
+                delete tmp;
+                return root;
+            }
+        }
+        if(root->val > key) root->left = deleteNode(root->left, key);
+        if(root->val < key) root->right = deleteNode(root->right, key);
+        return root;
     }
 };
--- maxDepth2/maxDepth2.cpp
+++ maxDepth2/maxDepth2.cpp
@@ -1,10 +1,41 @@
+#include <algorithm>
 #include <iostream>
-
+#include <stack>
+#include <vector>
 using namespace std;
 
 class Solution {
-public:
+   public:
     int maxDepth(string s) {
-
+        stack<char> st;
+        vector<int> depth;
+        int minDepth = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(s[i]);
+                depth.push_back(st.size());
+            }
+            if (s[i] == ')') {
+                st.pop();
+            }
+        }
+        if (!st.empty()) {
+            return 0;
+        } else {
+            for (int i = 0; i < depth.size(); i++) {
+                // cout << depth[i] << endl;
+                if (depth[i] >= minDepth) {
+                    minDepth = depth[i];
+                }
+            }
+            return minDepth;
+        }
     }
 };
+
+int main() {
+    string s = "1";
+    Solution solve;
+    int ret = solve.maxDepth(s);
+    cout << ret << endl;
+}
