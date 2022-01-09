38c34aea889ae35fbdf3a804007167a8f29405c5
tcxia <muctianciking@163.com>
2022-01-09 08:47:12 +0800

    2022-01-09

--- grayCode/grayCode.cpp
+++ grayCode/grayCode.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 class Solution {
    public:
+   //  16 >= n >= 1
     vector<int> grayCode(int n) {
+        vector<int> ret;
+        ret.reserve(1 << n); // 
+        ret.push_back(0);
+        for (int i = 1; i <= n; i++) {
+            int m = ret.size();
+            for (int j = m - 1; j >= 0; j--) {
+                ret.push_back(ret[j] | (1 << (i - 1)));
+            }
+        }
+        return ret;
     }
 };
---
+++ slowestKey/slowestKey.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+class Solution {
+   public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+    }
+};
