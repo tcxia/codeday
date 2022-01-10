bc76de43bbf46a5796c06d1ce8a06c0d981c4a97
tcxia <muctianciking@163.com>
2022-01-11 07:34:09 +0800

    2022-01-11

---
+++ isEscapePossible/isEscapePossible.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+using namespace std;
+class Solution {
+   public:
+    bool isEscapePossible(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
+    }
+};
--- maxDepth/maxDepth.cpp
+++ maxDepth/maxDepth.cpp
@@ -1,34 +1,35 @@
-#include <queue>
 #include <iostream>
+#include <queue>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
+#include "../common/binaryTree.h"
 class Solution {
     vector<vector<int>> result;
-    public:
+
+   public:
+    // 迭代法，采用层次遍历
     int maxDepth(TreeNode *root) {
-        queue<TreeNode*> que;
-        if(root != NULL) que.push(root);
-        while(!que.empty()){
+        queue<TreeNode *> que;
+        if (root != NULL) que.push(root);
+        while (!que.empty()) {
             int size = que.size();
             vector<int> temp;
-            for(int i = 0; i < size; i++){
-                TreeNode* node = que.front();
+            for (int i = 0; i < size; i++) {
+                TreeNode *node = que.front();
                 que.pop();
                 temp.push_back(node->val);
-                if(node->left) que.push(node->left);
-                if(node->right) que.push(node->right);
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
             }
             result.push_back(temp);
         }
         return result.size();
+    }
+
+    int maxDepthv2(TreeNode *root) {
+        if (root == nullptr) return 0;
+        // if (root->left == nullptr && root->right == nullptr) return 1;
+        int depth = max(maxDepthv2(root->left), maxDepthv2(root->right)) + 1;
+        return depth;
     }
 };
