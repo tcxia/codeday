2a29609dec07334eb2a0efa4f3aae62a47daee3b
tcxia <muctianciking@163.com>
2022-01-14 07:36:30 +0800

    2022-01-14

---
+++ findLength/findLength.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+using namespace std;
+class Solution {
+   public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+    }
+};
---
+++ kSmallestPairs/kSmallestPairs.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+using namespace std;
+class Solution {
+   public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+    }
+};
