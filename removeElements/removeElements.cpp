2ffe6796e993998db0a7613e593115d41bbdb0b7
tcxia <muctianciking@163.com>
2022-01-16 13:41:22 +0800

    2022-01-16

---
+++ removeElements/removeElements.cpp
@@ -0,0 +1,22 @@
+#include "../common/listNode.h"
+
+class Solution {
+   public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        while (cur->next != nullptr) {
+            if (cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+    }
+};
