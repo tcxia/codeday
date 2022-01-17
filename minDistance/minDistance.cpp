75ab5bbdc8b46aaafee0b0df94a3a31d7bb5ae3a
tcxia <muctianciking@163.com>
2022-01-17 13:41:24 +0800

    2022-01-17

--- minDistance/minDistance.cpp
+++ minDistance/minDistance.cpp
@@ -1,7 +1,29 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
+
+/**
+ * dp[i][j] 表示到i-1和j-1下标数组的删除的步骤数
+ * if (word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1]
+ * if (word1[i-1] != word2[j-1]) dp[i][j] = min(dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + 2)
+ *
+ */
 class Solution {
    public:
     int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1));
+        for (int i = 0; i <= word1.size(); i++) dp[i][0] = i;
+        for (int j = 0; j <= word2.size(); j++) dp[0][j] = j;
+        for (int i = 1; i <= word1.size(); i++) {
+            for (int j = 1; j <= word2.size(); j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else {
+                    dp[i][j] = min({dp[i - 1][j - 1] + 2, dp[i - 1][j] + 1, dp[i][j - 1] + 1});
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
--- numDistinct/numDistinct.cpp
+++ numDistinct/numDistinct.cpp
@@ -1,7 +1,38 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
+
+/**
+ * dp[i][j]: 表示 [0, i-1] 为结尾的s的子序列 和 以[0, j-1]为结尾的t的出现个数 dp[i][j]
+ *
+ * if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + 1
+ * else dp[i][j] = max(dp[i-1][j] + 1, dp[i][j-1] + 1)
+ *
+ * dp[i][j]: dp[i][0] = 1, dp[0][j] = 1
+ *
+ * 从前往后遍历
+ *
+ * dp[s.size()][t.size()] == dp[t.size()]
+ *
+ */
+
 class Solution {
    public:
     int numDistinct(string s, string t) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for (int i = 0; i < s.size(); i++) dp[i][0] = 1;
+        for (int j = 1; j < t.size(); j++) dp[0][j] = 0;
+
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= t.size(); j++) {
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[s.size()][t.size()];
     }
 };
