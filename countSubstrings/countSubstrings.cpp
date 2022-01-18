45f0b8a090b49d4e3067e8f9e1c1cad918b87d1e
tcxia <muctianciking@163.com>
2022-01-18 13:30:01 +0800

    2022-01-18

--- countSubstrings/countSubstrings.cpp
+++ countSubstrings/countSubstrings.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+/**
+ * dp[i][j]表示区间[i, j]是否为回文
+ * if(s[i] != s[j])  dp[i][j] false
+ * else{
+ *  if i == j dp[i][j] true
+ *  if j-i == 1 dp[i][j] true
+ *  if j -i > 1 dp[i][j] = dp[i+1][j-1]
+ * }
+ */
 class Solution {
+    bool isPalindromic(string s) {
+        int ls = 0;
+        int rs = s.size() - 1;
+        while (ls <= rs) {
+            if (s[ls] != s[rs]) {
+                return false;
+            } else {
+                ls++;
+                rs++;
+            }
+        }
+        return true;
+    }
+
    public:
     int countSubstrings(string s) {
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        int ret = 0;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (int j = i; j < s.size(); j++) {
+                if (s[i] == s[j]) {
+                    if (j - i <= 1) {
+                        ret++;
+                        dp[i][j] = true;
+                    } else if (dp[i + 1][j - 1]) {
+                        ret++;
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return ret;
     }
 };
--- longestPalindromeSubseq/longestPalindromeSubseq.cpp
+++ longestPalindromeSubseq/longestPalindromeSubseq.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+/**
+ * dp[i][j]表示区间[i,j]之间的最长回文子串
+ * if (s[i] == s[j]) dp[i][j] = dp[i+1][j-1] + 2
+ * else dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+ *
+ *
+ *
+ */
 class Solution {
    public:
     int longestPalindromeSubseq(string s) {
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 0));
+        for (int i = 0; i < s.size(); i++) dp[i][i] = 1;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.size(); j++) {
+                if (s[i] == s[j])
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.size() - 1];
     }
 };
