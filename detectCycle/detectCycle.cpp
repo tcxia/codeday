e1f8c7ab3183e983daf391401d88ea87d2470b77
tcxia <muctianciking@163.com>
2022-01-20 13:37:59 +0800

    2022-01-20

--- detectCycle/detectCycle.cpp
+++ detectCycle/detectCycle.cpp
@@ -1,8 +1,23 @@
 #include "../common/listNode.h"
 
 class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        
+   public:
+    ListNode* detectCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                ListNode* index1 = fast;
+                ListNode* index2 = head;
+                while (index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
+        }
+        return nullptr;
     }
 };
