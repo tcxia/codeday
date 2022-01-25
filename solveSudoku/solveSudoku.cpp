cb720a3bcbca69ed421ebc86b9ae023bed1a885c
tcxia <muctianciking@163.com>
2022-01-25 13:13:15 +0800

    2022-01-25

--- solveSudoku/solveSudoku.cpp
+++ solveSudoku/solveSudoku.cpp
@@ -26,7 +26,7 @@ class Solution {
     bool backtracking(vector<vector<char>>& board) {
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
-                if (board[i][j] == '.') continue;
+                if (board[i][j] != '.') continue;
                 for (char k = '1'; k <= '9'; k++) {
                     if (isValid(i, j, k, board)) {
                         board[i][j] = k;
@@ -45,3 +45,21 @@ class Solution {
         backtracking(board);
     }
 };
+
+
+
+int main(){
+    vector<vector<char>> board = {{'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}};
+
+
+    Solution solve;
+    solve.solveSudoku(board);
+}
