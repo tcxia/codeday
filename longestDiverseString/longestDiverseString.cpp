38e5663c5c0aa23e4b21b3d70c85e8bfde92fe85
tcxia <muctianciking@163.com>
2022-02-08 15:29:25 +0800

    2022-02-08

--- longestDiverseString/longestDiverseString.cpp
+++ longestDiverseString/longestDiverseString.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 class Solution {
+    // 一般规律，aabbccaabbcc
+    // aabbaaccaa
 public:
     string longestDiverseString(int a, int b, int c) {
-        
+        string res;
+        vector<pair<int, char>> arr = {{a, 'a'}, {b, 'b'}, {c, 'c'}};
+
+        while(true){
+            sort(arr.begin(), arr.end(), [](const pair<int, char>& p1, const pair<int,char>& p2){
+                return p1.first > p2.first;
+            });
+
+            bool hasNext = false;
+            for(auto & [freq, ch] : arr){
+                int m = res.size();
+                if (freq <= 0){
+                    break;
+                }
+                if (m >= 2 && res[m-2] == ch && res[m-1] == ch){
+                    continue;
+                }
+                hasNext = true;
+                res.push_back(ch);
+                freq--;
+                break;
+            }
+            if (!hasNext){
+                break;
+            }
+        }
+        return res;
     }
 };
