af6377e36ef9518a1197206b0465c831c34c4a43
tcxia <muctianciking@163.com>
2022-02-20 19:05:43 +0800

    2022-02-20

---
+++ 717.1-比特与-2-比特字符.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=717 lang=cpp
+ *
+ * [717] 1比特与2比特字符
+ */
+
+// @lc code=start
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+
+    }
+};
+// @lc code=end
+
