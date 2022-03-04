e2f5600675efdd49c89f6ee4b2d440d30813cc0f
tcxia <muctianciking@163.com>
2022-03-04 15:30:20 +0800

    2022-03-04

---
+++ 2104.子数组范围和.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=2104 lang=cpp
+ *
+ * [2104] 子数组范围和
+ */
+
+// @lc code=start
+#include <vector>
+
+
+using namespace std;
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
---
+++ 23.合并k个升序链表.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=cpp
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+    }
+};
+// @lc code=end
