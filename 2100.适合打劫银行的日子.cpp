38c06aa05bb20ed916c83044baf06eeb10efca60
tcxia <muctianciking@163.com>
2022-03-06 19:31:21 +0800

    2022-03-06

---
+++ 2100.适合打劫银行的日子.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=2100 lang=cpp
+ *
+ * [2100] 适合打劫银行的日子
+ */
+
+// @lc code=start
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> goodDaysToRobBank(vector<int>& security, int time) {
+    }
+};
+// @lc code=end
