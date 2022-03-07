e91d2f56df67ab9d6258972bb9e8f3d1d12626aa
tcxia <muctianciking@163.com>
2022-03-07 17:59:42 +0800

    2022-03-07

--- 2104.子数组范围和.cpp
+++ 2104.子数组范围和.cpp
@@ -5,16 +5,33 @@
  */
 
 // @lc code=start
+#include <algorithm>
+#include <iostream>
 #include <vector>
-
 
 using namespace std;
 
 class Solution {
-public:
+    
+
+   public:
     long long subArrayRanges(vector<int>& nums) {
-        
+        long long ret;
+        for(int i = 0; i < nums.size(); i++){
+            int minv = INT32_MAX, maxv = INT32_MIN;
+            for(int j = i; j < nums.size(); j++){
+                minv = min(minv, nums[j]);
+                maxv = max(maxv, nums[j]);
+                ret += (maxv - minv);
+            }
+        }
+        return ret;
     }
 };
 // @lc code=end
 
+int main() {
+    vector<int> nums = {1, 2, 3};
+    Solution solve;
+    solve.subArrayRanges(nums);
+}
