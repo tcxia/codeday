b87dfea2fc2c38ec941cb8ae462c937cc22d42dd
tcxia <muctianciking@163.com>
2022-03-09 20:52:17 +0800

    2022-03-09

---
+++ 303.区域和检索-数组不可变.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=cpp
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+
+    }
+    
+    int sumRange(int left, int right) {
+
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+// @lc code=end
+
