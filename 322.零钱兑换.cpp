9929d2b9cc4b7408ccb0c4a29a449114f06eea58
tcxia <muctianciking@163.com>
2022-04-07 13:27:47 +0800

    2022-04-07

--- 322.零钱兑换.cpp
+++ 322.零钱兑换.cpp
@@ -13,7 +13,17 @@ using namespace std;
 class Solution {
    public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT8_MAX);
+        vector<int> dp(amount + 1, INT32_MAX);
+        dp[0] = 0;
+        for (int i = 0; i < coins.size(); i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                if (dp[j - coins[i]] != INT32_MAX) {
+                    dp[j] = min(dp[j - coins[i]] + 1, dp[j]);
+                }
+            }
+        }
+        if (dp[amount] == INT32_MAX) return -1;
+        return dp[amount];
     }
 };
 // @lc code=end
