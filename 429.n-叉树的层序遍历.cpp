5c2fdca05305b922e6e6d1371149c7edb51384d7
tcxia <muctianciking@163.com>
2022-04-08 13:28:32 +0800

    2022-04-08

---
+++ 429.n-叉树的层序遍历.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=429 lang=cpp
+ *
+ * [429] N 叉树的层序遍历
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+*/
+
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Node {
+   public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+   public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> result;
+        queue<Node*> que;
+        if (root) que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            vector<int> onelevel;
+            for (int i = 0; i < size; i++) {
+                Node* cur = que.front();
+                que.pop();
+                onelevel.push_back(cur->val);
+                for (int j = 0; j < cur->children.size(); j++) {
+                    que.push(cur->children[j]);
+                }
+            }
+            result.push_back(onelevel);
+        }
+        return result;
+    }
+};
+// @lc code=end
