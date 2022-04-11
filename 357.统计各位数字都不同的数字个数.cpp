5b55d540901eb3dd1efa3c6d972a9455d17338e4
tcxia <muctianciking@163.com>
2022-04-11 18:34:27 +0800

    2022-04-11

--- 357.统计各位数字都不同的数字个数.cpp
+++ 357.统计各位数字都不同的数字个数.cpp
@@ -7,6 +7,11 @@
 // @lc code=start
 class Solution {
    public:
+   /*
+   当n=1时，满足条件的结果为10
+   当n=2时，即查找满足1位数和2位数，一位数是10，满足聊天的两位数从1到9，结果为10 + 9 * 9
+   当n=3时，即查找满足的1位数，2位数和3位数，即10 + 9 * 9 + 9 * 9 * 8
+   */
     int countNumbersWithUniqueDigits(int n) {
         if (n == 0) return 1;
         int sum = 10;
