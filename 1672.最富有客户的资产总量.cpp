767179d9a21efb0d18639248f6bd69d648a05389
tcxia <muctianciking@163.com>
2022-04-14 17:52:23 +0800

    2022-04-14

---
+++ 1672.最富有客户的资产总量.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=1672 lang=cpp
+ *
+ * [1672] 最富有客户的资产总量
+ */
+
+// @lc code=start
+
+#include <vector>
+using namespace std;
+
+
+class Solution {
+   public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int m = accounts.size(); // 行
+        int n = accounts[0].size(); // 列
+    }
+};
+// @lc code=end
