90c319bf86ae06543fd56912a6385459afc9d6ac
tcxia <muctianciking@163.com>
2022-04-14 17:54:25 +0800

    2022-04-14

--- 1672.最富有客户的资产总量.cpp
+++ 1672.最富有客户的资产总量.cpp
@@ -9,12 +9,22 @@
 #include <vector>
 using namespace std;
 
-
 class Solution {
    public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int m = accounts.size(); // 行
-        int n = accounts[0].size(); // 列
+        int m = accounts.size();     // 行
+        int n = accounts[0].size();  // 列
+        int maxValue = 0;
+        for (int i = 0; i < m; i++) {
+            int one = 0;
+            for (int j = 0; j < n; j++) {
+                one += accounts[i][j];
+            }
+            if (one > maxValue) {
+                maxValue = one;
+            }
+        }
+        return maxValue;
     }
 };
 // @lc code=end
