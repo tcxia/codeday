b90179c10a3726fa9dcf2c619a7077a4bbadeca4
tcxia <muctianciking@163.com>
2022-05-30 20:47:54 +0800

    2022-05-30

---
+++ 1022.从根到叶的二进制数之和.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=1022 lang=cpp
+ *
+ * [1022] 从根到叶的二进制数之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    int dfs(TreeNode* root, int val){
+        if (root == nullptr){
+            return 0;
+        }
+        val = (val << 1) | root->val;
+        if (root->left == nullptr && root->right == nullptr){
+            return val;
+        }
+        return dfs(root->left, val) + dfs(root->right, val);
+    }
+    int sumRootToLeaf(TreeNode *root) {
+        return dfs(root, 0);
+    }
+};
+// @lc code=end
