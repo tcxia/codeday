0c882fdae47fc8ed750ccf5b3a45770e8645aaee
tcxia <muctianciking@163.com>
2022-06-08 13:57:52 +0800

    2022-06-08

--- 367.有效的完全平方数.cpp
+++ 367.有效的完全平方数.cpp
@@ -5,22 +5,42 @@
  */
 
 // @lc code=start
+
+#include <iostream>
+
+using namespace std;
 class Solution {
-public:
-    bool isPerfectSquare(int num) {
-        int low = 0, high = num;
-        while(low <= high){
+   public:
+    bool isPerfectSquarev2(int num) {
+        int low = 1, high = num;
+        while (low <= high) {
             int mid = low + ((high - low) >> 1);
-            if ((long)mid * mid == num){
+            cout << mid << "," << high  << ","<< low<< endl;
+            if ((long)mid * mid == num) {
                 return true;
-            }else if ((long)mid * mid > num){
+            } else if ((long)mid * mid > num) {
                 high = mid - 1;
-            }else{
+            } else {
                 low = mid + 1;
             }
         }
         return false;
     }
+
+    // 如果一个数是完全平方数，那么它的前n个连续奇数的和 = n^2
+    bool isPerfectSquare(int num){
+        int i = 1;
+        while (num > 0){
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
 };
 // @lc code=end
 
+int main() {
+    Solution solve;
+    bool ret = solve.isPerfectSquare(5);
+    cout << ret << endl;
+}
--- 69.x-的平方根.cpp
+++ 69.x-的平方根.cpp
@@ -12,18 +12,22 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        int low = 0, high = x;
-        int res = -1;
+        if (x <= 1) return x;
+
+        int low = 1, high = x;
         while (low <= high){
             int mid = low + ((high - low) >> 1);
-            if (mid * mid <= x){
-                res = mid;
-                low = mid + 1;
-            }else if (mid * mid > x){
-                high = mid - 1;
+            if (mid == x / mid){  // 不能使用mid * mid 和 x比较，比较 mid 和 x/mid
+                return mid;
+            }else {
+                if (mid < x / mid){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
             }
         }
-        return res;
+        return high;
     }
 };
 // @lc code=end
