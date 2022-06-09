9ffa918d62b87981dd6106c2c51e27a20b80f38b
tcxia <muctianciking@163.com>
2022-06-09 15:22:02 +0800

    2022-06-09

--- 497.非重叠矩形中的随机点.cpp
+++ 497.非重叠矩形中的随机点.cpp
@@ -17,15 +17,19 @@ class Solution {
     vector<vector<int>>& rects;
     mt19937 gen{random_device{}()};
 
+    int num;
+
    public:
     Solution(vector<vector<int>>& rects) : rects{rects} {
-        this->arr.emplace_back(0);
-        for (auto& rect : rects) {
-            this->arr.emplace_back(arr.back() + (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1));
+        // this->arr.emplace_back(0);
+        // for (auto& rect : rects) {
+        //     this->arr.emplace_back(arr.back() + (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1));
+        // }
+        this->num = rects.size();
+        this->rects = rects;
         }
-    }
 
-    vector<int> pick() {
+    vector<int> pickv2() {
         uniform_int_distribution<int> dis(0, arr.back() - 1);
         int k = dis(gen) % arr.back();
         int rectIndex = upper_bound(arr.begin(), arr.end(), k) - arr.begin() - 1;
@@ -36,6 +40,13 @@ class Solution {
         int da = k / col;
         int db = k - col * da;
         return {a + da, b + db};
+    }
+
+    vector<int> pick(){
+        int choice = rand() % num;
+        int aX = rects[choice][2] - rects[choice][0] == 0 ? 0 : rand() % (rects[choice][2] - rects[choice][0]);
+        int aY = rects[choice][3] - rects[choice][1] == 0 ? 0 : rand() % (rects[choice][3] - rects[choice][1]);
+        return {rects[choice][0] + aX, rects[choice][1] + aY};
     }
 };
 
