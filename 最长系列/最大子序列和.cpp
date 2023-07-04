e9085f4e07c348a5476adb820eccdcfc2c1a2451
tcxia <muctianciking@163.com>
2023-07-04 17:49:35 +0800

    最长系列

--- 最长系列/最大子序列和.cpp
+++ 最长系列/最大子序列和.cpp
@@ -49,10 +49,10 @@ class Solution{
         int count = 0;
         for (int i = 0; i < nums.size(); ++i){
             count += nums[i];
-            if (count > result){
+            if (count > result){ // 取区间累计的最大值(相当于不断确定最大子序列终止位置)
                 result = count;
             }
-            if (count <= 0) count = 0;
+            if (count <= 0) count = 0; // 相当于重置最大子序列起始位置，因为遇到负数一定是拉低总和的
         }
         return result;
     }
